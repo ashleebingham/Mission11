dba4df2213066b15e3a632b3cc24fe94a6ba43cd
ashleebingham <ashleeb9@byu.edu>
2025-03-19 17:10:26 -0600

    initial

---
+++ backend/Mission11.API/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission11.API.Data;
+
+namespace Mission11.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookstoreContext _bookContext;
+        public BookController(BookstoreContext temp)
+        {
+            _bookContext = temp;
+        }
+
+        [HttpGet("AllBooks")]
+        public IEnumerable<Book> GetBooks()
+        {
+            return _bookContext.Books.ToList();
+        }
+
+    }
+}
---
+++ backend/Mission11.API/Data/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission11.API.Data;
+
+public partial class Book
+{
+    [Key]
+    public int BookId { get; set; }
+    [Required]
+    public required string Title { get; set; }
+    [Required]
+    public required string Author { get; set; }
+    [Required]
+    public required string Publisher { get; set; }
+    [Required]
+    public required string Isbn { get; set; }
+    [Required]
+    public required string Classification { get; set; }
+    [Required]
+    public required string Category { get; set; }
+    [Required]
+    public required int PageCount { get; set; }
+    [Required]
+    public required double Price { get; set; }
+}
