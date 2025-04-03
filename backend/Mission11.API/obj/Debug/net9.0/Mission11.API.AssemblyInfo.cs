d075c147264727de5a8c6602d2b5502363deec05
ashleebingham <ashleeb9@byu.edu>
2025-04-03 16:22:39 -0600

    here we go

--- backend/Mission11.API/obj/Debug/net9.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/Debug/net9.0/Mission11.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d806ee1ca9ac8317697198e9e88e75aae8e212a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61d7ca68d46ce9c81676807ebe7a20bae620f935")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
